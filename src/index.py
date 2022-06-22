8bd7b7971f8d5036416b0267d3ac5a8af4dad47e
rajesh-tota <108001568+rajesh-tota@users.noreply.github.com>
2022-06-22 19:01:41 +0530

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,29 +1,17 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-app = Flask(__name__)
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Rajesh Tota's new Website</title>
+</head>
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+<body>
+  <main>
+    <h1>Rajesh Tota's new Website is here!</h1>
+  </main>
+</body>
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-   app.run()
+</html>
